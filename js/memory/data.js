701e0612861de1cf01bfb5e1b73819efe48fd0b4
fengbo123 <shouyouk@163.com>
2015-08-19 19:26:26 +0800

    到一阶段

---
+++ js/memory/data.js
@@ -0,0 +1,97 @@
+var data = [];
+
+data.push({
+    'date'  : '2015-04-05',
+    'intro' : '奥体中心 and 鸟巢',
+    'media' : '<img src="images/memory/2015-04-05-2.jpg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2015-04-05',
+    'intro' : '奥体中心 and 鸟巢',
+    'media' : '<img src="images/memory/2015-04-05-1.jpg" width="370" >',
+    'like' : 238,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2015-04-05',
+    'intro' : '雨儿街',
+    'media' : '<img src="images/memory/2015-04-05.jpg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+
+data.push({
+    'date'  : '2015-02-24',
+    'intro' : '百天啦',
+    'media' : '<img src="images/memory/2015-02-24.jpg" width="370" >',
+    'like' : 32102,
+    'comment' : 883
+})
+
+
+data.push({
+    'date'  : '2015-02-17',
+    'intro' : '一起去滑雪 噜啦啦',
+    'media' : '<img src="images/memory/2015-02-17.jpg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2015-01-31',
+    'intro' : '惹您老人家生气了 对不起',
+    'media' : '',
+    'like' : 35038,
+    'comment' : 817
+})
+
+data.push({
+    'date'  : '2014-12-07',
+    'intro' : '可爱吧 不过可恶竟然删了我一张更好的照片，所以咧欠我一张特殊的照片哦 ，啦啦啦',
+    'media' : '<img src="images/memory/2014-12-07.jpg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-11-16',
+    'intro' : '表白哦',
+    'media' : '<img src="images/memory/2014-11-16.jpg" width="370">',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-11-03',
+    'intro' : 'fall in love',
+    'media' : '<img src="images/memory/2014-11-03.jpg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-10-23',
+    'intro' : '画的跟小狗一样',
+    'media' : '<img src="images/memory/2014-10-23.jpg" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+
+data.push({
+    'date'  : '2014-10-05',
+    'intro' : '第一次一起看电影哦',
+    'media' : '<img src="images/memory/2014-10-05.png" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
+data.push({
+    'date'  : '2014-10-03',
+    'intro' : '啦啦啦',
+    'media' : '<img src="images/memory/2014-10-03.png" width="370" >',
+    'like' : 25038,
+    'comment' : 417
+})
